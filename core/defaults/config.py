ec29968b7459274f0e9bb50abeb392b166ff27d8
p4ulbr4dl3y <54704531+p4ulbr4dl3y@users.noreply.github.com>
2026-08-13 04:11:27 +0300

    refactor(defaults): single ignore dirs source

--- core/defaults/config.py
+++ core/defaults/config.py
@@ -11,25 +11,6 @@ THEME_SECONDARY = "#f4f4f5"
 THEME_MUTED = "#71717a"
 THEME_SUBTLE = "#e4e4e7"
 
-# Standard Ignored Directories
-DEFAULT_IGNORE_DIRS = {
-    ".git",
-    ".johnston",
-    "venv",
-    ".venv",
-    "env",
-    ".env",
-    "node_modules",
-    "__pycache__",
-    ".pytest_cache",
-    ".mypy_cache",
-    ".ruff_cache",
-    "dist",
-    "build",
-    ".next",
-    ".nuxt",
-}
-
 # Permissions Defaults
 DEFAULT_PERMISSIONS = {
     "default": "allow",
